86d5c00d0fb691a4252765a076c9ba7f922128eb
André devkcud Albanese Junior <patommmmm@proton.me>
2023-12-23 04:22:58 -0300

    add missing prisma import

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -1,3 +1,5 @@
+import { prisma } from '$lib/server/prisma';
+
 export const load = async ({ params }: { params: { id: string } }) => {
   return { article: await prisma.article.findUnique({ where: { id: Number(params.id) } }) };
 };
